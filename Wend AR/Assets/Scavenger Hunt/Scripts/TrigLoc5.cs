add9c8e0812babd07b43310e7785aeeee4d36cba
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2020-02-17 19:08:40 -0600

    Added Project Folder, SDD and Presentation

---
+++ Wend AR/Assets/Scavenger Hunt/Scripts/TrigLoc5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using HutongGames.PlayMaker;
+
+public class TrigLoc5 : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    private readonly List<TrackableBehaviour.Status> mTrackableList = new List<TrackableBehaviour.Status>()
+    {
+        TrackableBehaviour.Status.DETECTED,
+        TrackableBehaviour.Status.TRACKED,
+
+        TrackableBehaviour.Status.EXTENDED_TRACKED
+    };
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        foreach (TrackableBehaviour.Status trackedStatus in mTrackableList)
+        { 
+            if (newStatus == trackedStatus)
+            {
+                PlayMakerFSM.BroadcastEvent("TriggerLocation5");
+            }
+        }
+    }
+}
